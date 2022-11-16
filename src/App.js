b45ba2bf9c5bcd32b30bf90e2d05f8049a6fabcc
xuanbao95 <bao.lgx@gmail.com>
2022-11-16 20:58:08 +0700

    create reducer router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     Nike Learning
-    </div>
+    <BrowserRouter>
+      <Routes>
+          {/* <Route/> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
