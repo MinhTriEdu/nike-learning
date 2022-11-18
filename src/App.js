898166cf0d0ee46a237202995b8ef376e6432276
TrinhHoaiDuc <dangnhap2030@gmail.com>
2022-11-18 10:56:10 +0700

    commit sang t6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path={"/"} element={<HomePage/>}/>
+          <Route path={"/home"} element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
   );
